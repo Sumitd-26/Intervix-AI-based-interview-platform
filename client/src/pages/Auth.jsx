db1999e9f98c6d3f2b05fcc3b1f663ab5e488656
Sumitd-26 <179369929+Sumitd-26@users.noreply.github.com>
2026-06-10 17:00:00 +0530

    Initial project setup

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Auth() {
+  return (
+    <div className="w-full min-h-screen bg-[#f3f3f3] flex items-center justify-center px-6 py-20">
+      <div className="w-full max-w-md p-8 roundeed-3xl bg-white shadow-2xl border border-gray-200"></div>
+    </div>
+  );
+}
+
+export default Auth;
