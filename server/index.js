74d98b16af90935ff82160080d7b1fe0d11e3dc3
Sumit dhiman <179369929+Sumitd-26@users.noreply.github.com>
2026-07-10 15:06:42 +0530

    Update CORS

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "https://intervix.onrender.com/",
+    origin: "https://intervix.onrender.com",
     credentials: true,
   }),
 );
